e97e548d7d7bdca97faf4d7333fa239c9b38f90c
yxie326 <yxie326@gatech.edu>
2022-11-12 02:47:42 -0500

    final check

--- src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
+++ src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
@@ -57,13 +57,11 @@ final class ProfilerImpl implements Profiler {
   }
 
   @Override
-  public void writeData(Path path) {
+  public void writeData(Path path) throws IOException {
     // TODO: Write the ProfilingState data to the given file path. If a file already exists at that
     //       path, the new data should be appended to the existing file.
     try (BufferedWriter writer = Files.newBufferedWriter(path)) {
       writeData(writer);
-    } catch (IOException e) {
-      e.printStackTrace();
     }
   }
 
